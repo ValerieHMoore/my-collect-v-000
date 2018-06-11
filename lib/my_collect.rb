8b13836dafc3541734db86e956a89507de8ca09a
Valerie Moore <periwinkle17@outlook.com>
2018-06-11 21:46:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect(array)
   if block_given?
+    new_array = []
     i = 0
     while i < array.length
-      yield array[i]
+      new_array.push yield array[i]
       i = i + 1
     end
     array
